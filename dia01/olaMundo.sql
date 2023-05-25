7e798b71a7ab3be14cf3ecba3713a131fecbcadb
slivonmp5@hotmail.com <slivonmp5@hotmail.com>
2023-05-25 22:02:27 +0000

    olá mundo do primeiro dia de sql essencials
    aqui fizemos criação de tabela no sql via comando select e pesquisa usando também o select from utilizando os dados so pizza query. tarefa realizada dentro dos clusters do databricks

---
+++ dia01/olaMundo.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+select 'olá mundo'
+
+-- COMMAND ----------
+
+select *                       -- selecione todas as colunas
+from silver.pizza_query.pedido -- da tabela silver.pizza.query.pediddo
